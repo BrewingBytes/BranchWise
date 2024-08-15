3c92467d58beec807536882dbc5a6e7f995ecb3c
Andrei Serban <andrei.serban@brewingbytes.com>
2024-08-15 17:49:44 +0300

    add vue tests

---
+++ src/tests/main.test.ts
@@ -0,0 +1,30 @@
+import { test, expect, vi } from "vitest";
+import { createApp } from "vue";
+import App from "../App.vue";
+
+test('App is mounted', () => {
+    document.body.innerHTML =
+        '<div id="app">' +
+        '</div>';
+
+
+    import('../main').then(() => {
+        const appElement = document.getElementById('app');
+        expect(appElement).toBeTruthy();
+    });
+});
+
+test('Plugins are registered', () => {
+    import('../plugins').then(({ default: registerPlugins }) => {
+        const app = createApp(App);
+        vi.spyOn(app, 'use');
+
+        expect(registerPlugins(app)).toHaveBeenCalled();
+
+        expect(app.use).toHaveBeenCalled();
+
+        import('../plugins/vuetify').then(({ default: vuetify }) => {
+            expect(app.use).toHaveBeenCalledWith(vuetify);
+        });
+    });
+});
