4a4318b748445bdbd0342d6cf2fbd3d23ffb1aad
Andrei Serban <andrei.serban@qorvo.com>
2024-08-15 10:16:11 +0300

    modify vitest

--- src/vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-    test: {
-        reporters: ['json', 'json-summary', 'text'],
-        outputFile: './dist/test-output.json'
-    }
-});
