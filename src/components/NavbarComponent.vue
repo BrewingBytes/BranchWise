a21ed6ff2f1052fbec534fbe32a48556382193c8
Andrei Serban <andrei.serban@brewingbytes.com>
2024-08-22 12:55:37 +0300

    fix lint

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,30 +1,34 @@
 <template>
-    <v-navigation-drawer v-model="isOpen">
-      <v-list style="height:100%; display: flex; flex-direction: column">
-        <v-list-item>
-          <v-row>
-            <v-col cols="3">
-              <v-icon icon="mdi:mdi-source-branch" />
-            </v-col>
-            <v-col cols="9">
-              <p class="text-h5">BranchWise</p>
-            </v-col>
-          </v-row>
-        </v-list-item>
-        <v-divider />
-        <v-spacer />
-        <v-list-item @click="closeMe">
-          <v-row>
-            <v-col cols="3">
-              <v-icon icon="mdi:mdi-arrow-left" />
-            </v-col>
-            <v-col cols="9">
-              <p class="text-h5">Close</p>
-            </v-col>
-          </v-row>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+  <v-navigation-drawer :model-value="isOpen">
+    <v-list style="height:100%; display: flex; flex-direction: column">
+      <v-list-item>
+        <v-row>
+          <v-col cols="3">
+            <v-icon icon="mdi:mdi-source-branch" />
+          </v-col>
+          <v-col cols="9">
+            <p class="text-h5">
+              BranchWise
+            </p>
+          </v-col>
+        </v-row>
+      </v-list-item>
+      <v-divider />
+      <v-spacer />
+      <v-list-item @click="closeMe">
+        <v-row>
+          <v-col cols="3">
+            <v-icon icon="mdi:mdi-arrow-left" />
+          </v-col>
+          <v-col cols="9">
+            <p class="text-h5">
+              Close
+            </p>
+          </v-col>
+        </v-row>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script lang="ts">
