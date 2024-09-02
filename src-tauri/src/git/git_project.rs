1d9db456eeb3318da2d9b378279b7b1093c8abdd
Andrei Serban <andrei.serban@brewingbytes.com>
2024-09-03 00:18:48 +0300

    add git branch test

--- src-tauri/src/git/git_project.rs
+++ src-tauri/src/git/git_project.rs
@@ -251,3 +251,15 @@ impl GitProject {
         &self.directory
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_git_branch_new() {
+        let branch = GitBranch::new("test".to_string(), "test".to_string());
+        assert_eq!(branch.name, "test");
+        assert_eq!(branch.commit, "test");
+    }
+}
