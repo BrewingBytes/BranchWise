fcf6b62bd543dc1877895f3aea39f83eb1d7e2fd
Andrei Serban <andrei.serban@brewingbytes.com>
2024-09-05 18:02:03 +0300

    add git author from string

--- src-tauri/src/git/git_commit.rs
+++ src-tauri/src/git/git_commit.rs
@@ -4,11 +4,12 @@ use flate2::bufread::ZlibDecoder;
 
 use crate::errors::git_commit_error::GitCommitError;
 
-use super::{git_commit_author::GitCommitAuthor, git_user::GitUser};
+use super::git_commit_author::GitCommitAuthor;
 
 pub struct GitCommit {
     hash: String,
-    parent_hash: Option<String>,
+    tree_hash: String,
+    parent_hashes: Vec<String>,
     author: GitCommitAuthor,
     committer: GitCommitAuthor,
     message: String,
@@ -17,14 +18,16 @@ pub struct GitCommit {
 impl GitCommit {
     pub fn new(
         hash: String,
-        parent_hash: Option<String>,
+        tree_hash: String,
+        parent_hashes: Vec<String>,
         author: GitCommitAuthor,
         committer: GitCommitAuthor,
         message: String,
     ) -> GitCommit {
         GitCommit {
             hash,
-            parent_hash,
+            tree_hash,
+            parent_hashes,
             author,
             committer,
             message,
@@ -41,22 +44,28 @@ impl GitCommit {
         zlib.read_to_string(&mut decoded_file_content)
             .map_err(|_| GitCommitError::DecompressionError)?;
 
-        let author = GitCommitAuthor::new(
-            GitUser::new("name".to_string(), "email".to_string()),
-            140122,
-            "0000".to_string(),
-        );
-        let committer = GitCommitAuthor::new(
-            GitUser::new("name".to_string(), "email".to_string()),
-            140122,
-            "0000".to_string(),
-        );
+        let mut lines = decoded_file_content.lines();
+        let tree_line = lines.next().ok_or(GitCommitError::InvalidCommitFile)?;
+        let tree_hash = tree_line
+            .strip_prefix("tree ")
+            .ok_or(GitCommitError::InvalidCommitFile)?;
+        let parent_hashes = lines.clone()
+            .take_while(|line| line.starts_with("parent "))
+            .map(|line| line.strip_prefix("parent ").unwrap().to_string())
+            .collect();
+        let author_line = lines.next().ok_or(GitCommitError::InvalidCommitFile)?;
+        let author = GitCommitAuthor::from_string(author_line)?;
+        let committer_line = lines.next().ok_or(GitCommitError::InvalidCommitFile)?;
+        let committer = GitCommitAuthor::from_string(committer_line)?;
+        let message = lines.collect::<Vec<&str>>().join("\n");
+
         Ok(GitCommit::new(
-            commit_hash.clone(),
-            Some(commit_hash),
+            commit_hash,
+            tree_hash.to_string(),
+            parent_hashes,
             author,
             committer,
-            "message".to_string(),
+            message,
         ))
     }
 }
@@ -66,9 +75,8 @@ mod tests {
     use super::*;
 
     use crate::git::git_user::GitUser;
-    use std::fs;
 
-    fn create_encoded_commit_file(commit_hash: &str, author: GitCommitAuthor, commiter: GitCommitAuthor, tree: Option<&str>, parent_commits: Vec<&str>, message: &str) -> Vec<u8> {
+    fn create_encoded_commit_file(author: GitCommitAuthor, commiter: GitCommitAuthor, tree: Option<&str>, parent_commits: Vec<&str>, message: &str) -> Vec<u8> {
         let tree_line = match tree {
             Some(tree) => format!("tree {}\n", tree),
             None => "".to_string(),
@@ -95,11 +103,10 @@ mod tests {
         }, 1725372312, "+0300".to_string());
 
         let commit_hash = "ae575432e84a11c11b8dc3e357806f65c50f4619".to_string();
-        let encoded_file_content = create_encoded_commit_file(&commit_hash, commiter.clone(), commiter, Some("50c8353444afbef3172c999ef6cff8d31309ac3e"), Vec::new(), "test commit");
+        let encoded_file_content = create_encoded_commit_file(commiter.clone(), commiter, Some("50c8353444afbef3172c999ef6cff8d31309ac3e"), Vec::new(), "test commit");
 
         let git_commit = GitCommit::from_string(commit_hash.clone(), &encoded_file_content).unwrap();
         assert_eq!(git_commit.hash, commit_hash);
-        assert_eq!(git_commit.parent_hash, Some(commit_hash));
     }
 
     #[test]
@@ -110,17 +117,19 @@ mod tests {
         let timezone = "timezone".to_string();
         let git_commit_author = GitCommitAuthor::new(git_user, 1, timezone.clone());
         let hash = "hash".to_string();
-        let parent_hash = "parent_hash".to_string();
+        let parent_hash = Vec::new();
         let message = "message".to_string();
         let git_commit = GitCommit::new(
             hash.clone(),
-            Some(parent_hash.clone()),
+            hash.clone(),
+            parent_hash.clone(),
             git_commit_author.clone(),
             git_commit_author.clone(),
             message.clone(),
         );
         assert_eq!(git_commit.hash, hash);
-        assert_eq!(git_commit.parent_hash, Some(parent_hash));
+        assert_eq!(git_commit.tree_hash, hash);
+        assert_eq!(git_commit.parent_hashes, parent_hash);
         assert_eq!(git_commit.author.get_user().name, name);
         assert_eq!(git_commit.author.get_user().email, email);
         assert_eq!(git_commit.committer.get_user().name, name);
--- src-tauri/src/git/git_commit_author.rs
+++ src-tauri/src/git/git_commit_author.rs
@@ -1,3 +1,5 @@
+use crate::errors::git_commit_error::GitCommitError;
+
 use super::git_user::GitUser;
 
 #[derive(Clone)]
@@ -15,6 +17,20 @@ impl GitCommitAuthor {
     pub fn get_user(&self) -> &GitUser {
         &self.user
     }
+
+    pub fn from_string(author_line: &str) -> Result<GitCommitAuthor, GitCommitError> {
+        let mut parts = author_line.split_whitespace();
+        let name = parts.next().ok_or(GitCommitError::InvalidCommitFile)?;
+        let email = parts.next().ok_or(GitCommitError::InvalidCommitFile)?.strip_prefix('<').ok_or(GitCommitError::InvalidCommitFile)?.strip_suffix('>').ok_or(GitCommitError::InvalidCommitFile)?;
+        let date_seconds = parts
+            .next()
+            .ok_or(GitCommitError::InvalidCommitFile)?
+            .parse()
+            .map_err(|_| GitCommitError::InvalidCommitFile)?;
+        let timezone = parts.next().ok_or(GitCommitError::InvalidCommitFile)?;
+
+        Ok(GitCommitAuthor::new(GitUser::new(name.to_string(), email.to_string()), date_seconds, timezone.to_string()))
+    }
 }
 
 #[cfg(test)]
@@ -25,6 +41,15 @@ mod tests {
     fn test_new() {
         let git_user = GitUser::new("name".to_string(), "email".to_string());
         let git_commit_author = GitCommitAuthor::new(git_user, 1, "timezone".to_string());
+        assert_eq!(git_commit_author.get_user().name, "name".to_string());
+        assert_eq!(git_commit_author.get_user().email, "email".to_string());
+        assert_eq!(git_commit_author.date_seconds, 1);
+        assert_eq!(git_commit_author.timezone, "timezone".to_string());
+    }
+
+    #[test]
+    fn test_from_string() {
+        let git_commit_author = GitCommitAuthor::from_string("name <email> 1 timezone").unwrap();
         assert_eq!(git_commit_author.get_user().name, "name".to_string());
         assert_eq!(git_commit_author.get_user().email, "email".to_string());
         assert_eq!(git_commit_author.date_seconds, 1);
