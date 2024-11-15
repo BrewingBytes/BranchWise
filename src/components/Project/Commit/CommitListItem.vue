794fe186dcb1e0eb4234ae070000afd394fc719c
Andrei Serban <andrei.serban@brewingbytes.com>
2024-11-15 16:00:13 +0200

    add commit list

---
+++ src/components/Project/Commit/CommitListItem.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-row
+    no-gutters
+    align="center"
+    style="height: 10vh;"
+  >
+    <v-col
+      style="height: 85%;"
+      cols="2"
+    >
+      <v-avatar
+        size="40"
+        color="blue"
+        class="text-white"
+      >
+        {{ getAuthorName[0] }}
+      </v-avatar>
+    </v-col>
+    <v-col
+      style="height: 95%;"
+      cols="10"
+    >
+      <v-row
+        no-gutters
+        style="height: 50%;"
+      >
+        <p class="text-blue-grey">
+          {{ getAuthorName }}
+        </p>
+        <v-spacer />
+        <p class="text-blue-lighten-3">
+          {{ getDate }}
+        </p>
+      </v-row>
+      <v-row
+        no-gutters
+        style="height: 50%; overflow-y: hidden;"
+      >
+        <p class="text-blue-grey">
+          {{ getHash }}
+        </p>
+        <v-spacer />
+        
+        {{ getMessage }}
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { getHash, IGitCommit } from '@/types/gitCommit';
+import moment from 'moment-timezone';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CommitListItem',
+  props: {
+    commit: {
+      type: Object as () => IGitCommit,
+      required: true,
+    },
+  },
+  computed: {
+    getAuthorName() {
+      return this.commit.author.user.name;
+    },
+    getDate() {
+        return moment.unix(this.commit.author.date_seconds).tz(this.commit.author.timezone).format('YYYY-MM-DD');
+    },
+    getHash() {
+      return getHash(this.commit);
+    },
+    getMessage() {
+        // Get the first line of the commit message based on the screen size
+        const message = this.commit.message.split('\n')[0];
+
+        let maxMessageLength = screen.width / 80;
+        return message.length > maxMessageLength ? message.slice(0, maxMessageLength) + '...' : message;
+    }
+  }
+});
+</script>
--- src/components/Project/CommitHistory.vue
+++ src/components/Project/CommitHistory.vue
@@ -1,11 +1,32 @@
 <template>
-  Commits here pls
+  <v-virtual-scroll
+    :height="getHeight"
+    :items="commits"
+  >
+    <template #default="{ item }">
+      <CommitListItem :commit="item" />
+    </template>
+  </v-virtual-scroll>
 </template>
 
 <script lang="ts">
+import CommitListItem from '@/components/Project/Commit/CommitListItem.vue';
+import { useProjectStore } from '@/stores/project';
+import { mapState } from 'pinia';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'CommitHistory',
+  components: {
+    CommitListItem,
+  },
+  computed: {
+    getHeight() {
+      return window.innerHeight - 64;
+    },
+    ...mapState(useProjectStore, {
+      commits: 'history',
+    }),
+  },
 });
 </script>
