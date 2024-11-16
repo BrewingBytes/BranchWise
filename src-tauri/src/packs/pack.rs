c917fff9ea6efacc153ea9d8cb16995d92a47f2b
Andrei Serban <andrei.serban@brewingbytes.com>
2024-11-16 23:39:24 +0200

    parse_reference_delta

--- src-tauri/src/packs/pack.rs
+++ src-tauri/src/packs/pack.rs
@@ -2,6 +2,8 @@ use std::{fs, io::Read, path::PathBuf};
 
 use flate2::bufread::ZlibDecoder;
 
+use crate::{database::storage::DATABASE, git::object::HASH_SIZE, packs::get_object_encoded_data};
+
 const HEADER_BYTES_V2: [u8; 8] = [0x50, 0x41, 0x43, 0x4B, 0, 0, 0, 2];
 const HEADER_BYTES_V3: [u8; 8] = [0x50, 0x41, 0x43, 0x4B, 0, 0, 0, 3];
 
@@ -51,7 +53,7 @@ pub fn get_encoded_data_from_pack(path: &PathBuf, offset: usize) -> Vec<u8> {
 
     match object_type {
         GitPackType::ObjectOffsetDelta => todo!(),
-        GitPackType::ObjectReferenceDelta => todo!(),
+        GitPackType::ObjectReferenceDelta => parse_reference_delta(&data[1..], size),
         GitPackType::Invalid => panic!(),
         _ => {
             let mut decoded_data: Vec<u8> = vec![0; size];
@@ -62,6 +64,119 @@ pub fn get_encoded_data_from_pack(path: &PathBuf, offset: usize) -> Vec<u8> {
             decoded_data
         }
     }
+}
+
+fn parse_reference_delta(data: &[u8], size: usize) -> Vec<u8> {
+    let hash = &data[..HASH_SIZE];
+
+    let (source_size, source_shift) = variable_length_int(&data[HASH_SIZE..]);
+    let (target_size, target_shift) = variable_length_int(&data[HASH_SIZE + source_shift..]);
+
+    let mut delta_instructions: Vec<u8> = vec![0; size - source_shift - target_shift - HASH_SIZE];
+    ZlibDecoder::new(&data[HASH_SIZE + source_shift + target_shift..])
+        .read_exact(&mut delta_instructions)
+        .unwrap();
+
+    let project = DATABASE.lock().unwrap().get_current_project().unwrap();
+    let source = &get_object_encoded_data(&project, &hex::encode(hash)).unwrap_or_default();
+    if source.len() != source_size {
+        panic!();
+    }
+
+    let mut target = Vec::<u8>::new();
+
+    let mut delta_instructions = delta_instructions.as_slice();
+    while delta_instructions.is_empty() {
+        let instruction = delta_instructions[0];
+
+        if instruction & 0b1000_0000 != 0 {
+            // Copy Instruction
+            let offset_1 = (instruction & 0b1) == 0b1;
+            let offset_2 = (instruction & 0b10) == 0b10;
+            let offset_3 = (instruction & 0b100) == 0b100;
+            let offset_4 = (instruction & 0b1000) == 0b1000;
+            let size_1 = (instruction & 0b1_0000) == 0b1_0000;
+            let size_2 = (instruction & 0b10_0000) == 0b10_0000;
+            let size_3 = (instruction & 0b100_0000) == 0b100_0000;
+
+            let mut offset: usize = 0;
+            let mut size: usize = 0;
+            // Skip the instruction byte
+            delta_instructions = &delta_instructions[1..];
+
+            // Parse the offset and size
+            // and advance the delta instructions
+            if offset_1 {
+                offset = delta_instructions[0] as usize;
+                delta_instructions = &delta_instructions[1..];
+            }
+
+            if offset_2 {
+                offset |= (delta_instructions[0] as usize) << 8;
+                delta_instructions = &delta_instructions[1..];
+            }
+
+            if offset_3 {
+                offset |= (delta_instructions[0] as usize) << 16;
+                delta_instructions = &delta_instructions[1..];
+            }
+
+            if offset_4 {
+                offset |= (delta_instructions[0] as usize) << 24;
+                delta_instructions = &delta_instructions[1..];
+            }
+
+            if size_1 {
+                size = delta_instructions[0] as usize;
+                delta_instructions = &delta_instructions[1..];
+            }
+
+            if size_2 {
+                size |= (delta_instructions[0] as usize) << 8;
+                delta_instructions = &delta_instructions[1..];
+            }
+
+            if size_3 {
+                size |= (delta_instructions[0] as usize) << 16;
+                delta_instructions = &delta_instructions[1..];
+            }
+
+            // If the size is set to 0, it means max size
+            if size == 0 {
+                size = 0x10000;
+            }
+
+            // Copy the data from the source to the target
+            target.extend_from_slice(&source[offset..offset + size]);
+        } else if instruction != 0 {
+            // Insert Instruction
+            let add_size = (instruction & 0b0111_1111) as usize;
+            target.extend_from_slice(&delta_instructions[1..add_size + 1]);
+
+            delta_instructions = &delta_instructions[add_size + 1..];
+        }
+    }
+
+    if target.len() != target_size {
+        panic!();
+    }
+
+    target
+}
+
+fn variable_length_int(data: &[u8]) -> (usize, usize) {
+    let mut data = data;
+
+    let mut size: usize = (data[0] & 0b0111_1111) as usize;
+    let mut shift: usize = 7;
+
+    while data[0] & 0b1000_0000 != 0 {
+        data = &data[1..];
+        size |= ((data[0] & 0b0111_1111) as usize) << shift;
+        shift += 7;
+    }
+
+    (size, shift / 7)
 }
 
 fn is_header_valid(header: &[u8]) -> bool {
