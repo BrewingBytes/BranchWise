5f6cc2d4fcd3be1ae19707def18d9cc86595716c
Andrei Serban <andrei.serban@brewingbytes.com>
2024-11-16 21:04:37 +0200

    add comms

--- src-tauri/src/packs/index.rs
+++ src-tauri/src/packs/index.rs
@@ -5,6 +5,14 @@ use crate::git::object::HASH_SIZE;
 
 const HEADER_BYTES: [u8; 8] = [0xff, 0x74, 0x4f, 0x63, 0, 0, 0, 2];
 
+/**
+ * Check if the hash is in the index file
+ * 
+ * index: The path to the index file
+ * hash: The hash to check
+ * 
+ * Returns a tuple with a boolean if the hash is in the index and the offset of the object in the pack
+ */
 pub fn is_hash_in_index(index: &PathBuf, hash: &str) -> (bool, usize) {
     let data = fs::read(index);
     if data.is_err() || !is_header_valid(&data.as_ref().unwrap()[..8]) {
